58c141ca32634cc497c481f0b29ef67776a4e1b5
b21hamwe <b21hamwe@student.his.se>
2022-05-17 16:28:21 +0200

    Added my url where i'll later on fetch the data from my json code and added on the public class extends so that the jsonTask class may be read.

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21hamwe";
+    private String TAG = "==>";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onPostExecute(String json) {
     }
 }
