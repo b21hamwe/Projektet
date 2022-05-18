359281d7ef0c344117ad891597a9c54d04ce279a
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-18 19:43:54 +0200

    Created public member variables that'll connect to the item_holder xml file.

--- app/src/main/java/com/example/projekt/ViewHolder.java
+++ app/src/main/java/com/example/projekt/ViewHolder.java
@@ -1,9 +1,24 @@
 package com.example.projekt;
 
-public class ViewHolder {
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+public class ViewHolder extends RecyclerView.ViewHolder {
 
+    public TextView size;
+    public TextView name;
+    public TextView company;
+    public TextView id;
 
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
 
+        name = itemView.findViewById(R.id.blommaprint);
+        size = itemView.findViewById(R.id.sizeprint);
+        company = itemView.findViewById(R.id.companyprint);
+        id = itemView.findViewById(R.id.IDprint);
+    }
 }
