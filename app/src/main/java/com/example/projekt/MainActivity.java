3e1a1093d567f9e27a85fe450ca65eeb84c4bfb5
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-18 17:24:11 +0200

    Created private member variables in MainActivity.java

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+
+    private ArrayList<Blommor> blommorList;
+    private RecyclerView recyclerView;
+    private BlomAdapter adapter;
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21hamwe";
     private String TAG = "==>";
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new BlomAdapter();
+        blommorList = new ArrayList<blommor>();
+        recyclerView = findViewById(R.id. recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 
 
