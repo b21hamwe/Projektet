ae660103a4426cdae840775ad8f69c274a791864
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-19 15:50:54 +0200

    Removed redundance

--- app/src/main/java/com/example/projekt/Adapter.java
+++ app/src/main/java/com/example/projekt/Adapter.java
@@ -14,27 +14,28 @@ public class Adapter extends RecyclerView.Adapter<ViewHolder> {
     private List<Blommor> blommorList = new ArrayList();
 
 
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder (@NonNull ViewGroup parent,int viewType){
-            return new ViewHolder((LayoutInflater.from(parent.getContext()).inflate(R.layout.item_holder, parent, false)));
-        }
-
-        @Override
-        public void onBindViewHolder (@NonNull ViewHolder holder,int position){
-            Blommor blommor = blommorList.get(position);
-
-            holder.name.setText(blommor.name);
-            holder.size.setText(String.valueOf(blommor.getSize()));
-            holder.company.setText(blommor.company);
-        }
-
-        @Override
-        public int getItemCount (){return blommorList.size();};
-
-        public void setBlommor (List<Blommor>blommorList) {this.blommorList = blommorList;} {
-            this.blommorList = blommorList;
-
-        }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder((LayoutInflater.from(parent.getContext()).inflate(R.layout.item_holder, parent, false)));
     }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Blommor blommor = blommorList.get(position);
+
+        holder.name.setText(blommor.name);
+        holder.size.setText(String.valueOf(blommor.getSize()));
+        holder.company.setText(blommor.company);
+    }
+
+    @Override
+    public int getItemCount() {
+        return blommorList.size();
+    };
+
+    public void setBlommor(List<Blommor> blommorList) {
+        this.blommorList = blommorList;
+    };
+
+}
