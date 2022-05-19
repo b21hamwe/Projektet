f87558de813b03cf962094c8b4f66dfb73434cb9
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-19 13:48:47 +0200

    Used Getters and Setter plus changed a variable name to a better one

--- app/src/main/java/com/example/projekt/Blommor.java
+++ app/src/main/java/com/example/projekt/Blommor.java
@@ -2,8 +2,47 @@ package com.example.projekt;
 
 public class Blommor {
 
-        String blomma;
+        String name;
         Integer size;
         String Company;
         String ID;
+
+
+        public String getBlomma() {
+                return name;
+        }
+
+        public void setBlomma(String blomma) {
+                this.name = blomma;
+        }
+
+        public Integer getSize() {
+                return size;
+        }
+
+        public void setSize(Integer size) {
+                this.size = size;
+        }
+
+        public String getCompany() {
+                return Company;
+        }
+
+        public void setCompany(String company) {
+                Company = company;
+        }
+
+        public String getID() {
+                return ID;
+        }
+
+        public void setID(String ID) {
+                this.ID = ID;
+        }
+
+        public Blommor(String Name, Integer Size, String ID){
+                this.name = Name;
+                this.size = Size;
+                this.ID = ID;
+        }
 }
