592005ef38d4aa68dda0920bc9cb52c4fca683d6
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-20 16:49:25 +0200

    Added code and played around in order to connect my about page to be and external page

--- app/src/main/java/com/example/projekt/About.java
+++ app/src/main/java/com/example/projekt/About.java
@@ -3,6 +3,8 @@ package com.example.projekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public WebSettings getSettings() {
+    }
+
+    public void setWebViewClient(WebViewClient webViewClient) {
+    }
+
+    public void loadUrl(String s) {
     }
 }
--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebViewClient;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -19,23 +25,41 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private ArrayList<Blommor> blommorList;
     private RecyclerView recyclerView;
     private Adapter adapter;
+    private About webView;
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b21hamwe";
     private String TAG = "==>";
 
 
+    public void showExternalWebPage() {
+
+        webView.loadUrl("https://mobprog.webug.se/json-api?login=b21hamwe");
+    }
+
+    public void showInternalWebPage(){
+        webView.loadUrl("file:///android_asset/menu.xml");
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         adapter = new Adapter();
         blommorList = new ArrayList<Blommor>();
-        recyclerView = findViewById(R.id. recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+        webView = findViewById(R.id.web_View);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/menu.xml");
+
+
+
 
         new JsonTask(this).execute(JSON_URL);
     }
@@ -47,11 +71,43 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Type type = new TypeToken<List<Blommor>>() {
         }.getType();
 
-        List<Blommor> listOfblommorList = gson.fromJson(json,type);
+        List<Blommor> listOfblommorList = gson.fromJson(json, type);
         adapter.setblommorList(listOfblommorList);
         adapter.notifyDataSetChanged();
 
 
-
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_external_web) {
+            showExternalWebPage();
+            Log.d("==>", "Will display external web page");
+            return true;
+        }
+
+        if (id == R.id.action_internal_web) {
+            showInternalWebPage();
+            Log.d("==>", "Will display internal web page");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
