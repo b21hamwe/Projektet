60838b42145b1b5ed876ec91b1584f85ec135a6f
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-23 16:10:20 +0200

    Added intents that'll take us to the about page

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -89,7 +89,24 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+
+        if (id == R.id.action_internal_web) {
+
+            Intent intent = new Intent(MainActivity.this, About.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
